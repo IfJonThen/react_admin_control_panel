11b5f80db80e54c6f7a75dffc5bd158a4eb017c6
jonsenY <pikahatonjon@gmail.com>
2017-02-11 04:06:04 -0800

    remove member now correctly loads the list //to do refresh button functionality and styling

--- src/components/RosterForm.js
+++ src/components/RosterForm.js
@@ -4,98 +4,28 @@ import {Form,FormControl,ControlLabel,HelpBlock,FormGroup} from 'react-bootstrap
 import members from '../data/database';
 import {Button} from './ButtonGroup';
 import ReactFireMixin from 'reactfire';
-import * as firebase from 'firebase';
 import ReactDOM from 'react-dom';
+import Rebase from 're-base';
 /*eslint no-unused-vars: "off"*/
-import {base,firebaseAuth} from '../static/js/auth';
-
+import {firebaseAuth} from '../static/js/firebaseAuth';
+// import base from '../static/js/firebaseAuth';
 // import * as firebase from 'firebase';
+var base = Rebase.createClass({
+    apiKey: "AIzaSyDbA2-3W4c4a1Fdl9QPG_KHMJGIRSn_ORU",
+    authDomain:"classexaminer.firebaseapp.com",
+    databaseURL:"https://classexaminer.firebaseio.com",
+    storageBucket:"classexaminer.appspot.com",
+    messagingSenderId:"150507520756"
+});
 
-// var fb = Rebase.createClass({apikey: "AIzaSyDbA2-3W4c4a1Fdl9QPG_KHMJGIRSn_ORU",
-//     authDomain:"classexaminer.firebaseapp.com",
-//     databaseURL:"https://classexaminer.firebaseio.com",
-//     storageBucket:"classexaminer.appspot.com",
-// });
-// var RosterForm = React.createClass({
-//     mixins:[ReactFireMixin],
-//     getInitialState: function() {
-//         return {
-//         };
-//     },
-//     onButtonClick(event){
-//         event.preventDefault();
-//         // this.firebaseRef.push({
-//         //    text:this.state.text
-//         // });
-//         // this.setState({text:""});
-//         console.log("add button clicked");
-//         let fname = document.getElementById("inputFname");
-//         let lname = document.getElementById("inputLname");
-//         fname = fname.value;
-//         lname= lname.value;
-//         let quarter = document.getElementById("selectPledgeYear");
-//         quarter = quarter.options[quarter.selectedIndex].text;
-//         let year = document.getElementById("inputYear").value;
-//         console.log("adding member", fname+ " " + lname+" "+quarter  + " "+ year);
-//
-//     },
-//     componentWillMount: function() {
-//         var firebaseRef = firebase.database().ref("Names");
-//         this.bindAsArray(firebaseRef.limitToLast(25), "Names");
-//     },
-//     render(){
-//         return(<div className="splitform"><Form id="rosterform">
-//                 <div className="form-group row">
-//                     <label className="col-sm-2 col-form-label">First Name</label>
-//                     <div className="col-sm-6">
-//                         <input type="name" className="form-control" id="inputFname" placeholder="First Name">
-//                         </input>
-//                     </div>
-//                 </div>
-//                 <div className="form-group row">
-//                     <label className="col-sm-2 col-form-label">Last Name</label>
-//                     <div className="col-sm-6">
-//                         <input type="name" className="form-control" id="inputLname" placeholder="Last Name">
-//                         </input>
-//                     </div>
-//                 </div>
-//                 <div className="form-group row">
-//                     <label  className="col-sm-2 col-form-label">Pledge Class</label>
-//                     <div className="col-sm-6">
-//                         <select className="form-control" id="selectPledgeYear">
-//                             <option>Fall</option>
-//                             <option>Winter</option>
-//                             <option>Spring</option>
-//                         </select>
-//                     </div>
-//                 </div>
-//                 <div className="form-group row">
-//                     <label className="col-sm-2 col-form-label">Year</label>
-//                     <div className="col-sm-6">
-//                         <input type="name" className="form-control" id="inputYear" placeholder="Year">
-//                         </input>
-//                     </div>
-//                 </div>
-//                 <Button onClick={this.onButtonClick} id="insertmemberbtn" value="Add">Add</Button>
-//             </Form>
-//             </div>
-//
-//         );
-//     }
-// });
 class RosterForm extends Component{
-    constructor(){
-        super();
-
-        // this.db = this.props.db;
+    constructor(props){
+        super(props);
+        console.log("RosterForm::constructor::props.count " +props.count);
     }
-
     onButtonClick(event){
         event.preventDefault();
-        // this.firebaseRef.push({
-        //    text:this.state.text
-        // });
-        // this.setState({text:""});
+
         console.log("add button clicked");
         let fname = document.getElementById("inputFname");
         let lname = document.getElementById("inputLname");
@@ -105,16 +35,27 @@ class RosterForm extends Component{
         quarter = quarter.options[quarter.selectedIndex].text;
         let year = document.getElementById("inputYear").value;
         console.log("adding member", fname+ " " + lname+" "+quarter  + " "+ year);
-        base.update('name',{
-            data:{name: fname},
+        //change this to push
+        // base.update('classes',{
+        //     data:{uid: {"jyuen":{f2k16:['CS 161','Informatics 133','Informatics 124']}}},
+        //     then(err){
+        //         if(!err){
+        //             alert("successfully added "+ fname)
+        //             // Router.transitionTo('')
+        //         }
+        //     }
+        // });
+        let t = {};
+        t[this.count]=fname;
+        base.update('users',{
+            data:t,
             then(err){
                 if(!err){
-                    alert("successfully added "+ fname)
+                    alert("RosterForm:buttonHandler: successfully added "+ fname)
                     // Router.transitionTo('')
                 }
             }
-            });
-
+        });
     }
     componentDidMount() {
         // var firebaseRef = firebase.database().ref("Names");
@@ -166,51 +107,99 @@ class RosterForm extends Component{
     }
 }
 export class RosterEdit extends Component{
-    constructor(){
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {users: [],base:[],count:props.count};
+        this.fetchData = this.fetchData.bind(this);
+        this.fetchData();
+        this.count = props.count;
+        this.handleRefresh=this.handleRefresh.bind(this);
+        this.handleRemoveBtn=this.handleRemoveBtn.bind(this);
+        this.pullList = this.pullList.bind(this)
+    }
+    fetchData(){
+        base.fetch('users', {context:this,asArray:true,
+            then(data){
+                if (this.state.count!==data.length){
+                    this.setState({base:data,count:data.length});
+                    this.count=data.length;
+                    console.log("RosterEdit::fetch:: count changed successfully");
+                    // this.pullList();
+
+                }
+                else{
+                    console.log("RosterEdit::fetch::count changed unsuccesfully");
+                    // this.pullList();
+
+                }
+                console.log("RosterEdit:: fetched data success " + data + data.length +this.state.count);
+
+            }});
     }
     handleRemoveBtn(){
         let v = document.getElementById("selectRemove");
         v=v.options[v.selectedIndex].text;
         console.log("RosterEdit::handleRemoveBtn::triggered :" +v +"will be removed");
-}
-    componentDidMount(){
-        // base.bindToState('names',{
-        //     context:this,
-        //     state:'tasks',
-        //     asArray:true
-        // });
     }
-    componentWillMount() {
-        //firebase -> fetch
-        //allows you to retrieve the data from firebase endpoint
-        // base.fetch('names', {
-        //     context: this,
-        //     asArray: true
-        // }).then(data => {
-        //     console.log(data)
-        // }).catch(error=>{})
-        //
+    handleRefresh(){
+        console.log("RosterEdit::handleRefresh::refreshing...");
+        setTimeout(this.pullList(),500);
+        // let v = document.getElementById("selectRemove");
+        // v=v.options[v.selectedIndex].text;
+        // console.log("RosterEdit::handleRemoveBtn::triggered :" +v +"will be removed");
     }
-    generate(){
-        let t=null;
-        for (let v =0; v<members["name"].length;v++){
-            console.log("RosterEdit::generate()::members['name']["+v+"]::"+members["name"][v]);
-            t=<option>{members["name"][v]}</option>;
+    createUID(fname,lname){
+        if(fname.length!==0 &&lname.length!==0) {
+            return fname[0]+lname;
         }
-        return t;
+    }
+    pullList(){
+        let v = null;
+        var t =document.getElementById("selectRemove");
+        console.log("RosterEdit: pullList: adding "+this.state.count +" options");
+        for(let j =0; j<this.state.count;j++){
+            if(typeof this.state.base[j]=== 'string') {
+                v = document.createElement("option");
+                v.appendChild(document.createTextNode(this.state.base[j]));
+                v.value = j + 1;
+                t.appendChild(v);
+            }
+        }
+        console.log("RosterEdit: pullList: added "+this.state.count +" options");
+    }
+    componentDidMount(){
+        console.log("RosterEdit did mount()");
+        // this.pullList();
+
+    }
+    componentWillMount(){
+        console.log("RosterEdit will mount()");
+        this.pullList();
+    }
+    componentWillUpdate(){
+        this.pullList();
+
+        if(this.count!==0){
+            // console.log("RosterEditWillUpdate : "+this.fetchData());
+        }
+        else{
+            console.log("database is empty");
+
+        }
     }
     render(){
         return (
             <div className="splitform">
                 <Form>
                     <div className="form-group">
-                    <label >Remove Member</label>
+                    <label id="editFormLabel">Remove Member</label>
                     <select className="form-control" id="selectRemove">
-                        {/*{this.generate()}*/}
+                        {this.props.right}
                     </select>
                         <Button onClick={this.handleRemoveBtn} id="removebtn" value="Remove"/>
-                </div>
+                        <Button onClick={this.handleRefreshBtn} id="refreshbtn" value="Refresh"/>
+
+                    </div>
 
                 </Form>
             </div>
--- src/components/RosterView.js
+++ src/components/RosterView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import '../static/css/MainView.css';
 import NavExample from "./Nav";
 import {Form, Grid} from 'react-bootstrap';
+import '../static/css/MainView.css';
+
 import SplitView from './SplitView';
 import TableView from './TableView';
 import {Button} from './ButtonGroup';
@@ -10,16 +11,25 @@ var members={'Name':'Jon,Max,Gary'};
 var memInfo= {'Gary':{},'Jon':{},'Max':{} };
 var table=[];
 
-import * as firebase from "firebase";
+import {base} from "../static/js/firebaseRef";
 /*eslint no-unused-vars: "off"*/
 
 
 class RosterView extends Component{
-    constructor(props){
-      super(props);
+
+    constructor(){
+      super();
+      this.count=0;
+      this.state={count:null,base:null};
+
       this.state={pane:"add"};
         this.handleSelectA=this.handleSelectA.bind(this);
         this.handleSelectR=this.handleSelectR.bind(this);
+        this.onChange=this.onChange.bind(this)
+        this.pullList=this.pullList.bind(this);
+        this.pullList2=this.pullList2.bind(this);
+        this.pullDB=this.pullDB.bind(this);
+        this.loadSections=this.loadSections.bind(this);
     }
     componentDidMount(){
 
@@ -27,14 +37,70 @@ class RosterView extends Component{
     componentWillUnMount(){
 
     }
+    pullDB(){
+        base.fetch('users', {
+            context: this, asArray: true,
+        }).then(data=>
+        {
+            this.count=data.length;
+            console.log("data contains " +data);
+            this.setState({base:data,count:data.length});
+        }).catch(error=>{
+            console.log("RosterView:constructor: fetch error");
+        });
+        console.log("RosterView::constructor::this.state.value " + this.state.count);
+    }
+    loadSections(){
+        this.pullList();
+    }
+    onChange(){
+        if (this.state.count===undefined){
+            this.pullDB();
+            return this.state.count;
+        }
+        else{
+            return this.state.count;
+        }
+        // this.pullList();
+    }
     handleSelectR(event){
-        console.log('RosterView::handleSelectRemove() clicked');
-        console.log("remove "+event.target.id);
+        // console.log('RosterView::handleSelectRemove() clicked');
         this.setState({pane:"remove"});
+        this.pullList();
     }
     handleSelectA(){
-        console.log('RosterView::handleSelectAdd() clicked');
+        // console.log('RosterView::handleSelectAdd() clicked');
         this.setState({pane:"add"});
+    }
+    pullList(){
+        let v = null;
+        var t =document.getElementById("selectRemove");
+        console.log("RosterView::pullList:: this.state.base:" +this.state.base);
+        if (t !=null) {
+            for (let j = 0; j < this.state.count; j++) {
+                if (typeof this.state.base[j] === 'string') {
+                    v = document.createElement("option");
+                    v.appendChild(document.createTextNode(this.state.base[j]));
+                    v.value = j + 1;
+                    t.appendChild(v);
+                }
+            }
+        }
+        console.log("RosterForm: pullList: added "+this.state.count +" options");
+    }
+    pullList2(){
+
+        let v = [];
+        var t =document.getElementById("selectRemove");
+        console.log("RosterView::pullList2:: this.state.base:" +this.state.base);
+        for(let j =0; j<this.state.count;j++){
+            if(typeof this.state.base[j]=== 'string') {
+               v.push(<option key={j}>{this.state.base[j]}</option>);
+            }
+        }
+        console.log("RosterForm: pullList2: added "+v+" options");
+
+        return v;
     }
     render() {
         let left =
@@ -44,15 +110,17 @@ class RosterView extends Component{
                 </div>
                 <div className="row">
                     <Button onClick={this.handleSelectR}id="Remove" value="Remove Member"/>
-
                 </div>
             </div>;
         let right =null;
+        let v = this.pullList2();
+
         if (this.state.pane==="add"){
-            right= <RosterForm db={this.props.db}/>;
+            let count= this.onChange();
+            right= <RosterForm onChange={count}/>;
         }
         else{
-            right=<RosterEdit db={this.props.db}/>;
+            right=<RosterEdit right={v} count={this.state.count}/>;
         }
 
         return (
@@ -64,6 +132,12 @@ class RosterView extends Component{
 
     }
 }
+RosterView.propTypes={
+    count:React.PropTypes.number
+};
+RosterView.defaultProps={
+    count:0
+};
 function SplitPane(props){
     return(<div className="SplitPane">
         <div className="SplitPane-left">
