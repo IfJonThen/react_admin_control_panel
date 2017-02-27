fd4f8944236012b39dad8944ecac7007ccbddcc5
IfJonThen <pikahatonjon@gmail.com>
2017-02-27 04:32:56 -0800

    **made MainView a bit more interesting to look at.

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -12,9 +12,10 @@ import MainSquares from './MainSquares';
 /*eslint no-unused-vars: "off"*/
 
 class MainView extends Component{
-    constructor(){
-        super();
-        this.classupdate=this.classupdate.bind(this);
+    constructor(props){
+        super(props);
+        this.state={nav:props.nv};
+        this.AttendanceUpdate=this.AttendanceUpdate.bind(this);
         this.rosterupdate=this.rosterupdate.bind(this);
         this.calupdate=this.calupdate.bind(this);
     }
@@ -31,18 +32,44 @@ class MainView extends Component{
         // browserHistory.push('home');
 
     }
+    AttendanceUpdate(){
+        this.setState({nav:"attendance"});
+        this.props.handleClick("home");
 
+    }
     render() {
-        let v = (<Grid fluid={true}>
-            <div className="MainSquare">
-                <Button onClick={this.rosterupdate} className="squares"id="rosterupdate" value="Roster Update"/>
-                <Button onClick={this.classupdate} className="squares" id="classupdate" value="Class Update"/>
-                <Button onClick={this.calupdate} className="squares" id="calupdate" value="Calendar Update"/>
-            </div>
-        </Grid>);
-
-       return (<div className="mainView">{v}
-
+        let main= null;
+        console.log("MainView()::render()"+ this.state.nav);
+        switch(this.state.nav){
+            case undefined:
+                main=<div className="MainSquare">
+                    <Button onClick={this.AttendanceUpdate} className="squares"id="attendanceupdate" value="Attendance"/>
+                    <Button onClick={this.classupdate} className="squares" id="classupdate" value="Class Update"/>
+                    <Button onClick={this.calupdate} className="squares" id="calupdate" value="Calendar Update"/>
+                </div>;
+                break;
+            case "attendance":
+                //to do attendance view
+                main=<div > code for attendance</div>;
+                break;
+            case "calendar":
+                main=<div> code for calendar</div>;
+                //to Do calendarView
+                break;
+            default:
+                main=<div className="MainSquare">
+                    <Button onClick={this.AttendanceUpdate} className="squares"id="attendanceupdate" value="Attendance"/>
+                    <Button onClick={this.classupdate} className="squares" id="classupdate" value="Class Update"/>
+                    <Button onClick={this.calupdate} className="squares" id="calupdate" value="Calendar Update"/>
+                </div>
+                break;
+        }
+       return (<div className="mainView">
+           <Grid fluid={true}>
+               <div className="MainSquare">
+                   {main}
+               </div>
+           </Grid>
        </div>);
     }
 }
