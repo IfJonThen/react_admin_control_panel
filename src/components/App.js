59e7dc614c2c2f5bea5fe914104ee1a1a47032f1
IfJonThen <pikahatonjon@gmail.com>
2017-04-11 20:50:04 -0700

    refactored App controller to handle different sources of state change more nicely, and included alpha-test feature flags

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,86 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../static/css/App.css';
-import NavExample from './Nav';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import {browserHistory,Router} from 'react-router';
-import {Grid} from 'react-bootstrap';
 import MainView from './MainView';
 import SplitView from './SplitView';
-import DropdownSection, {Section} from './DropdownSection';
-import {Button} from './ButtonGroup';
+import {ButtonGroup} from './ButtonGroup';
 import ClassTabView from './ClassTabView';
 import RosterView from './RosterView';
 import LogInControl from './LogInControl';
 import NavBar from './NavBar';
 import {base} from '../static/js/firebaseRef';
-import firebase from 'firebase';
 import {login,logout} from '../static/js/firebaseAuth';
 /*eslint no-unused-vars: "off"*/
 import {AuthService} from '../utils/AuthService';
 class App extends Component {
     constructor(){
         super();
-        this.state={cred:[],isLoggedIn:false,loc:undefined};
+        this.state={cred:[],isLoggedIn:false,loc:undefined,alpha:false,nav:'home'};
         this.logInCheck=this.logInCheck.bind(this);
         this.checkLogIn=this.checkLogIn.bind(this);
         this.handleNavClick=this.handleNavClick.bind(this);
+        this.handleMainClick=this.handleMainClick.bind(this);
         console.log("App.js: state.loc:"+this.state.loc);
         this.checkLogIn(["j","yuen"]);
-
-
+        this.person = null;
     }
     /*App()::handleNavClick():
     * helper function for NavBar. updates state
     * */
-    handleNavClick(nav){
-        if (nav==="logout"){
-            this.setState({isLoggedIn:false});
-        }
-        else if (nav==="attendance"){
-            this.setState({loc:"home"});
-        }
-        else {
-            this.setState({loc:nav});
+    handleMainClick(nav){
+        console.log(nav);
+        switch(nav){
+            case "AttendanceBtn":
+                this.setState({nav:nav});
+                break;
+            case "FormBtn":
+                alert("forms");
+                this.setState({nav:nav});
+                break;
+            case "CalBtn":
+                this.setState({nav:nav});
+                break;
+            default:
+                this.setState({nav:nav});
+                break;
         }
     }
-
-
+    handleNavClick(nav){
+        console.log(nav);
+        switch(nav){
+            case 'home':
+                this.setState({nav:nav,loc:nav});
+                break;
+            case 'database':
+                this.setState({loc:nav});
+                break;
+            case 'logout':
+                this.setState({isLoggedIn:false});
+                break;
+            case 'alpha':
+                this.setState({alpha:true});
+                break;
+            default:
+                this.setState({loc:nav});
+                break;
+        }
+    }
     /*App ():: logInCheck():
     * checks if user is logged in
     * */
     logInCheck(event){
-       //Note: The user's password is NOT the password used to access the user's email account.
-        // The email address serves as a unique identifier for the user, and the password is used to access the user's account in your Firebase project.
-
         this.checkLogIn(event);
-       //  firebase.auth().signInWithEmailAndPassword().catch(function(error){
-       //      let errorCode = error.code;
-       //      let errorMessage = error.message;
-       //      switch(errorCode){
-       //          case 'auth/wrongpassword':
-       //              alert("incorrect password");
-       //              break;
-       //          case 'auth/wrongpassword':
-       //              alert("incorrect password");
-       //              break;
-       //          case 'auth/wrongpassword':
-       //              alert("incorrect password");
-       //              break;
-       //          case 'auth/wrongpassword':
-       //              alert("incorrect password");
-       //              break;
-       //      }
-       // });
 
-        let login=false;
-        for (let i =0; i<this.state.cred.length;i++){
-            if (this.state.cred[i]["username"]===event[0]&& this.state.cred[i]["password"]===event[1]){
-                login=true;
-            }
-        }
+        // let login=false;
+        let login=true;
+        // for (let i =0; i<this.state.cred.length;i++){
+        //     if (this.state.cred[i]["username"]===event[0]&& this.state.cred[i]["password"]===event[1]){
+        //         login=true;
+        //         this.person=this.state.cred[i]["username"];
+        //     }
+        // }
         if (login){
             alert("Login Successful");
             this.setState({isLoggedIn:true,loc:"home"});
@@ -87,7 +88,6 @@ class App extends Component {
         }
         else{
             alert("Incorrect Username and Password");
-
         }
        //  this.setState({isLoggedIn:true,loc:"home"});
         //
@@ -101,8 +101,6 @@ class App extends Component {
             context: this, asArray: true,
         }).then(data=>
         {
-
-                // alert(data.length);
            this.setState({cred:data});
         }).catch(error=>{
             console.log("App::checkLogIn():: fetch error");
@@ -117,29 +115,31 @@ class App extends Component {
             navbar =(<div>
                     <NavBar handleClick={this.handleNavClick}/>
                 </div>);
-
             //LogIn render condition
-            if (this.state.loc === undefined){
-                document.body.style.backgroundColor="white";
-                navbar=<div id="loginTemp">
-                    <LogInControl isLoggedin={this.logInCheck}/>
-                </div>;
-            }
-            else {
-                //sets body to grey color if logged in
-                document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
-                if (this.state.loc === "roster") {
-                    var s = <Button id="AddM" value="Add Member"></Button>;
-                    var t = (<DropdownSection/>);
+            switch(this.state.loc){
+                default:
+                    document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
+                    break;
+                case "database":
+                    alert("database");
+                    break;
+                case undefined:
+                    alert("alert");
+                    document.body.style.backgroundColor="white";
+                    navbar=<div id="loginTemp">
+                        <LogInControl isLoggedin={this.logInCheck}/>
+                    </div>;
+                        break;
+                case "roster":
                     base = <RosterView base={this.state.base} db={this.props.db}/>;
-                }
-                //home render condition
-                else if (this.state.loc === "home") {
-                    base = <MainView handleClick={this.handleNavClick}nv={this.state.loc}/>;
-                }
-                else if (this.state.loc==="classes"){
+                    break;
+                case "home":
+                    base = <MainView handleClick={this.handleMainClick}nv={this.state.nav}/>;
+                    break;
+                case "classes":
                     base=<ClassTabView/>;
-                }
+                    break;
+
             }
         }
         else{
@@ -149,7 +149,7 @@ class App extends Component {
         return (<div className="App">
                         <div className="App-header">
                             <img src={logo} className="App-logo" alt="logo"/>
-                            <h2>Welcome</h2>
+                            <h2>Welcome {this.person}</h2>
                             </div>
                         {navbar}
                         {base}
