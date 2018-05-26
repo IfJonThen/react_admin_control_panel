8bb6ed4aba897da6fb141dc297aa505e88eae7d4
IfJonThen <pikahatonjon@gmail.com>
2018-05-26 01:48:49 -0700

    heh

--- src/static/js/firebaseRef.js
+++ src/static/js/firebaseRef.js
@@ -4,19 +4,11 @@ import Rebase from 're-base';
 // import * as firebase from 'firebase';
 
 export const base = Rebase.createClass({
-    apiKey: "AIzaSyDbA2-3W4c4a1Fdl9QPG_KHMJGIRSn_ORU",
-    authDomain:"classexaminer.firebaseapp.com",
-    databaseURL:"https://classexaminer.firebaseio.com",
-    storageBucket:"classexaminer.appspot.com",
-    messagingSenderId:"150507520756"
+    
 });
 // export base.database().ref();
 // export const firebaseAuth = base.auth();
 
 var config = {
-    apiKey: "AIzaSyDbA2-3W4c4a1Fdl9QPG_KHMJGIRSn_ORU",
-    authDomain: "classexaminer.firebaseapp.com",
-    databaseURL: "https://classexaminer.firebaseio.com",
-    storageBucket: "classexaminer.appspot.com",
-    messagingSenderId: "150507520756"
+    
 };
